eb2eb23847614a63c84774d9cbb9939411dbec93
Timothy Woodard <timothywoodard91@gmail.com>
2021-07-09 14:15:13 -0400

    first commit

---
+++ JavaScript2/loops.js
@@ -0,0 +1,37 @@
+/*
+
+for (let index = 0; index < 3; index++) {
+  console.log('hello world!');
+}
+
+*/
+let x = 6;
+switch (x) {
+  case 1:
+    console.log('too small');
+    break;
+
+  case 2:
+    console.log('too small');
+    break;
+
+  case 3:
+    console.log('too small');
+    break;
+  case 4:
+    console.log('exactly');
+    break;
+  case 5:
+    console.log('too large');
+    break;
+  default:
+    console.log('Input is out of range');
+}
+
+/*
+const numbers = [1, 2, 3, 4];
+
+numbers.forEach((number) => {
+  console.log(number + 1);
+});
+*/
